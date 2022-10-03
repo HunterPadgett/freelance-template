ba022c0eb5a6e1aec7ad0000abe5552bd0bf8911
Hunter Padgett <gizmokabanjo@gmail.com>
2022-10-03 17:03:34 -0400

    atttoryney

--- src/pages/Attorneys/BDS.js
+++ src/pages/Attorneys/BDS.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
+import "./Attorneys.css";
 
 const BDS = () => {
   return (
-    <div>BDS</div>
-  )
-}
+    <div className="">
+      <div className="name grid grid-cols-1 items-center text-center">
+        <h1 className="text-3xl md:text-5xl m-1" id="profileh1">
+          Benjamin D. Summerlin
+        </h1>
+        <h3 className="text-2xl md:text-4xl m-1">Associate</h3>
+      </div>
 
-export default BDS
+      <div className="grid grid-cols-1 ">
+        <img
+          class="profpic mx-auto mt-3"
+          src="https://mdbootstrap.com/img/new/standard/nature/182.jpg"
+          alt="sdfsdf"
+        />
+      </div>
+      <div className="container">
+        <div className="grid grid-cols-1 text-center">
+          <div className="m-2">
+            <h3 className="text-4xl md:text-5xl">Practice Areas</h3>
+            <ul className="md:text-lg">
+              <li>Commercial and Residential Real Estate</li>
+              <li>Business and Corporate Law</li>
+              <li>Maritime Law</li>
+              <li>Civil Litigation</li>
+              <li>Personal Injury</li>
+            </ul>
+          </div>
+          <div className="m-2">
+            <h3 className="text-4xl md:text-5xl">Education</h3>
+            <ul className="md:text-lg">
+              <li>Juris Doctor, 1988 – University of Georgia School of Law</li>
+              <li>Bachelor of Arts, 1985 – University of Georgia</li>
+              <li>Class of 1981 – Benedictine Military School</li>
+            </ul>
+          </div>
+          <div className="m-2">
+            <h3 className="text-4xl md:text-5xl">Legal Experience</h3>
+            <ul className="md:text-lg">
+              <li className="m-1">
+                Admitted to the State Bar of Georgia in 1988
+              </li>
+              <li className="m-1">
+                Chairman of Chatham County Board of Elections
+              </li>
+              <li className="m-1">
+                Admitted to practice in the U.S. Supreme Court, U.S. Court of
+                Appeals 4th and 11th Circuits, and the U.S. District Courts for
+                the Southern District of GA & SC
+              </li>
+              <li className="m-1">
+                Admitted to practice in the GA Supreme Court & GA Court of
+                Appeals
+              </li>
+              <li className="m-1">
+                Admitted to practice in the SC Supreme Court & SC Court of
+                Appeals
+              </li>
+              <li className="m-1">
+                Special Assistant Attorney General (SAAG) Georgia Ports
+                Authority 1987 – Present
+              </li>
+              <li className="m-1">
+                Special Assistant Attorney General for the GA Department of
+                Transportation 1999 – Present
+              </li>
+              <li className="m-1">
+                General Counsel, Benedictine Military School
+              </li>
+              <li className="m-1">
+                General Counsel, St. Patrick’s Day Parade Committee
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BDS;
