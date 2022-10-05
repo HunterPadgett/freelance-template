9d9aef7c1c1bf2775b70828dad8a4ab9b387a369
Hunter Padgett <gizmokabanjo@gmail.com>
2022-10-05 12:48:07 -0400

    bg fix

--- src/components/Footer/Menus.js
+++ src/components/Footer/Menus.js
@@ -1,6 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 export const ABOUT = [
   { name: "Example Link", link: "www.google.com" },
-  { name: "About Us", link: "#" },
+  { name: "About Us", link: '/practice-areas' },
 ];
 export const SERVICES = [
   { name: "Our Team", link: "/attorneys" },
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,15 +8,15 @@ export default function Contact() {
           <form target="_blank" action="https://formsubmit.co/676eb6ebd07f887811064705af76dfc2" method="POST">
             <div className="form-group" id="contactForm">
               <div className="form">
-                <div className="pb-2 col-lg-4 col-md-8 col-sm-12 mx-auto ">
+                <div className="pb-3 col-lg-4 col-md-8 col-sm-12 mx-auto ">
                   <input type="text" name="name" className="form-control justify-center items-center" placeholder="Name" required></input>
                 </div>
-                <div className="pb-2 col-lg-4 col-md-8 col-sm-12 mx-auto">
+                <div className="pb-3 col-lg-4 col-md-8 col-sm-12 mx-auto">
                   <input type="email" name="email" className="form-control " placeholder="Email" required></input>
                 </div>
                 <div className="form-group pb-3 text-center w- mx-auto">
                   <textarea placeholder="Your Message" className="form-control col-lg-7 col-sm-10 mx-auto" name="message" rows="10" required></textarea>
-                  <button type="submit" className="btn col-md-4 col-sm-10 mx-auto btn-lg subbtn btn-block hover:bg-blue-900 text-white mt-3">Submit Form</button>
+                  <button type="submit" className="btn col-md-3 col-sm-10 border border-transparent mx-auto btn-lg subbtn btn-block hover:bg-blue-900 text-white mt-3">Submit Form</button>
                 </div>
               </div>
             </div>
