d223d480512b68e68e15d4411755e57638964784
Hunter Padgett <gizmokabanjo@gmail.com>
2022-10-07 14:32:54 -0400

    history

--- src/pages/History/index.js
+++ src/pages/History/index.js
@@ -1,15 +1,103 @@
-// import thisS from "../../assets/blueLogo.png";
 import { Link } from "react-router-dom";
-import '../../App.css'
+import "../../App.css";
 
 export default function History() {
   return (
-    <div className="secondarybg">
-      <div className=" h-screen">
-      <h1 className="text-white">History</h1>
+    <div className="secondarybg bg-gray-300 bg-opacity-20">
+      <div className="grid grid-cols-1 items-center text-center mb-3">
+        <h1 className="text-white p-3 md:text-5xl">History</h1>
+      </div>
+      <div className="container">
+        <div className="">
+          <p className="text-white clear-none text-center md:clear-left mb-0 d-sm-none">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            cumque sunt, minus, nesciunt repudiandae suscipit ad impedit labore,
+            dolor laboriosam aut nisi! Alias est voluptatum nulla similique
+            incidunt possimus iusto. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Reiciendis cumque sunt, minus, nesciunt
+            repudiandae suscipit ad impedit labore, dolor laboriosam aut nisi!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            cumque sunt, minus, nesciunt repudiandae suscipit ad impedit labore,
+            dolor laboriosam aut nisi! Alias est voluptatum nulla similique
+            incidunt possimus iusto. Alias est voluptatum nulla similique
+            incidunt possimus iusto. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Reiciendis cumque sunt, minus, nesciunt
+            repudiandae suscipit ad impedit labore, dolor laboriosam aut nisi!
+            Alias est voluptatum nulla similique incidunt possimus iusto. Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Reiciendis cumque
+            sunt, minus, nesciunt repudiandae suscipit ad impedit labore, dolor
+            laboriosam!
+          </p>
+          <img
+            className="sm:float-none mx-auto md:mx-0 md:float-right md:p-2 max-w-xs md:max-w-sm order-last"
+            alt="dfg"
+            src="https://images.unsplash.com/photo-1665127178832-42aaf2f6640c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          ></img>
+          <p className="text-white clear-none text-center md:clear-left mb-0 d-none d-md-block">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            cumque sunt, minus, nesciunt repudiandae suscipit ad impedit labore,
+            dolor laboriosam aut nisi! Alias est voluptatum nulla similique
+            incidunt possimus iusto. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Reiciendis cumque sunt, minus, nesciunt
+            repudiandae suscipit ad impedit labore, dolor laboriosam aut nisi!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            cumque sunt, minus, nesciunt repudiandae suscipit ad impedit labore,
+            dolor laboriosam aut nisi! Alias est voluptatum nulla similique
+            incidunt possimus iusto. Alias est voluptatum nulla similique
+            incidunt possimus iusto. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Reiciendis cumque sunt, minus, nesciunt
+            repudiandae suscipit ad impedit labore, dolor laboriosam aut nisi!
+            Alias est voluptatum nulla similique incidunt possimus iusto. Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Reiciendis cumque
+            sunt, minus, nesciunt repudiandae suscipit ad impedit labore, dolor
+            laboriosam!
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="">
+          <p className="text-white clear-none text-center mb-0">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            cumque sunt, minus, nesciunt repudiandae suscipit ad impedit labore,
+            dolor laboriosam aut nisi! Alias est voluptatum nulla similique
+            incidunt possimus iusto. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Reiciendis cumque sunt, minus, nesciunt
+            repudiandae suscipit ad impedit labore, dolor laboriosam aut nisi!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            cumque sunt, minus, nesciunt repudiandae suscipit ad impedit labore,
+            dolor laboriosam aut nisi!
+          </p>
+        </div>
+      </div>
+      <div className="container pb-3">
+        <div className="">
+          <img
+            className="sm:float-none mx-auto md:mx-0 md:float-left md:p-2 max-w-xs md:max-w-sm"
+            alt="dfg"
+            src="https://images.unsplash.com/photo-1665127178832-42aaf2f6640c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          ></img>
+          <p className="text-white clear-none text-center md:clear-right mb-0">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            cumque sunt, minus, nesciunt repudiandae suscipit ad impedit labore,
+            dolor laboriosam aut nisi! Alias est voluptatum nulla similique
+            incidunt possimus iusto. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Reiciendis cumque sunt, minus, nesciunt
+            repudiandae suscipit ad impedit labore, dolor laboriosam aut nisi!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            cumque sunt, minus, nesciunt repudiandae suscipit ad impedit labore,
+            dolor laboriosam aut nisi! Alias est voluptatum nulla similique
+            incidunt possimus iusto. Alias est voluptatum nulla similique
+            incidunt possimus iusto. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Reiciendis cumque sunt, minus, nesciunt
+            repudiandae suscipit ad impedit labore, dolor laboriosam aut nisi!
+            Alias est voluptatum nulla similique incidunt possimus iusto.
+            incidunt possimus iusto. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Reiciendis cumque sunt, minus, nesciunt
+            repudiandae suscipit ad impedit labore, dolor laboriosam aut nisi!
+            Alias est voluptatum nulla similique incidunt possimus iusto.
+          </p>
+        </div>
       </div>
     </div>
   );
 }
-
-
